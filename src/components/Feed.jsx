d9ec9ffdc0a4f471f75083c7327c90e79a53b80d
saifgehlot07 <gehlotsaif07@gmail.com>
2023-07-09 18:39:07 +0530

    API issue

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +22,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -28,11 +41,11 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New
+          {selectedCategory}
           <span style={{ color: "#f31503", pl: 2 }}> Videos</span>
         </Typography>
       </Box>
-      <Videos videos={[]} />
+      <Videos videos={videos} />
     </Stack>
   );
 };
